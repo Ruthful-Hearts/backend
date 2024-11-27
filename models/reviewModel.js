15f7c33c63fedc9abf183ff77c125637d35ecaef
Okitta <okitta9388@gmail.com>
2024-11-27 22:15:46 +0300

    [Update]: update the model contents for the model files created.

---
+++ models/reviewModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+    store: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Store',
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      trim: true,
+    },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+reviewSchema.pre('save', function (next) {
+  if (!this.product && !this.store) {
+    return next(new Error('A review must be associated with either a product or a store.'));
+  }
+  next();
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
