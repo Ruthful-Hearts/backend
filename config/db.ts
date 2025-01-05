64b28a3b697b1a6d7939089897509e3b5145fbeb
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2025-01-05 14:05:36 +0300

    fix: fix default export error

--- config/db.ts
+++ config/db.ts
@@ -3,18 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB connected successfully!");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error.message);
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			//   useNewUrlParser: true,
+			//   useUnifiedTopology: true,
+		});
+		console.log("✅ MongoDB connected successfully!");
+	} catch (error) {
+		console.error("❌ MongoDB connection error:", error.message);
 
-    setTimeout(() => {
-      connectDB();
-    }, 5000);
-  }
+		setTimeout(() => {
+			connectDB();
+		}, 5000);
+	}
 };
+
+export default connectDB;
